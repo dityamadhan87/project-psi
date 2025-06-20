a6580f8de38d7755d6b7e220769a2395667796ff
Hafidz <hafidzartha_426@student.ub.ac.id>
2025-06-20 14:32:29 +0700

    audio & sleeping

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const meowlifeMachine = createMachine({
     initial: 'idle',
     context: {
         energy: 50,
+        energyAwal: 0, // <--- untuk menyimpan nilai awal sebelum tidur
         satiety: 50,
         cleanliness: 50,
         mood: 0,
@@ -92,7 +93,6 @@ const meowlifeMachine = createMachine({
                         }
                     ]
                 },
-                },
                 CLEAN_POOP: {
                     target: 'idle',
                     actions: [
@@ -117,7 +117,7 @@ const meowlifeMachine = createMachine({
                     target: 'idle',
                     actions: [
                         assign({
-                            xp: (context) => context.xp + 500,
+                            xp: (context) => context.xp + 100,
                             energy: (context) => Math.max(0, context.energy - 7),
                             satiety: (context) => Math.max(0, context.satiety - 10),
                             mood: (context) => Math.min(100, context.mood + 10)
@@ -128,7 +128,7 @@ const meowlifeMachine = createMachine({
         },
         eating: {
             after: {
-                5000: [
+                6000: [
                     {
                     target: "idle",
                     actions: [
@@ -142,34 +142,47 @@ const meowlifeMachine = createMachine({
             },
         },
         sleeping: {
+            entry: assign({
+                energyAwal: (ctx) => ctx.energy
+            }),
             invoke: {
                 id: 'sleepingInterval',
                 src: (context, event) => (callback, onReceive) => {
                     const interval = setInterval(() => {
                         callback('TICK');
                     }, 1000);
-
-                    return () => clearInterval(interval); // cleanup saat keluar dari state
+                    return () => clearInterval(interval);
                 }
             },
+            always: [
+                {
+                    cond: (ctx) => ctx.energy >= 100,
+                    target: 'idle',
+                    actions: assign({
+                        xp: (ctx) => ctx.xp + 200
+                    })
+                }
+            ],
             on: {
                 TICK: {
                     actions: assign({
                         energy: (ctx) => Math.min(100, ctx.energy + 5),
-                        satiety: (ctx) => Math.max(0, ctx.satiety - 5),
-                        cleanliness: (ctx) => Math.max(0, ctx.cleanliness - 8),
+                        mood: (ctx) => Math.min(100, ctx.mood + 2),
+                        satiety: (ctx) => Math.max(0, ctx.satiety - 2),
+                        cleanliness: (ctx) => Math.max(0, ctx.cleanliness - 4)
+                    })
+                },
+                WAKE_UP: {
+                    target: 'idle',
+                    actions: assign({
+                        xp: (ctx) => {
+                            const kenaikan = ctx.energy - ctx.energyAwal;
+                            return ctx.xp + Math.floor(kenaikan * 1.5);
+                        }
                     })
                 }
             },
-            after: {    
-                5000: { target: 'idle', actions: [
-                    assign({
-                        xp: (context) => context.xp + 200,
-                        mood: (ctx) => Math.min(100, ctx.mood + 25)
-                    })
-                ]},
-            }
-        },
+        },   
         bathing: {
             after: {
                 7000: {
@@ -266,12 +279,12 @@ function updateUI(state) {
     const feedBtn = document.getElementById('feed-btn');
     const dropdown = document.getElementById('dropdown-feed-options');
     const celebrate = document.getElementById('celebrate');
+    const wakeUp = document.getElementById('wake-up');
 
     // Toggle dropdown saat tombol diklik
     feedBtn.addEventListener('click', () => {
         dropdown.classList.toggle('hidden');
     });
-
     // Sembunyikan dropdown saat klik di luar area dropdown
     document.addEventListener('click', (event) => {
     if (!feedBtn.contains(event.target) && !dropdown.contains(event.target)) {
@@ -290,8 +303,11 @@ function updateUI(state) {
         bathBtn.classList.add('opacity-50', 'cursor-not-allowed');
         feedBtn.disabled = true;
         feedBtn.classList.add('opacity-50', 'cursor-not-allowed');
-    } else if (state.matches('sleeping')) {
+    } 
+    else if (state.matches('sleeping')) {
         cat.innerHTML = `<img src="./cat-sleep.png" alt="Cat Sleeping" />`;
+        new Audio('./cat-sleping.wav').play();
+        wakeUp.classList.remove('hidden');
         soundBtn.disabled = true;
         soundBtn.classList.add('opacity-50', 'cursor-not-allowed');
         sleepBtn.disabled = true;
@@ -303,6 +319,7 @@ function updateUI(state) {
     }
     else if (state.matches('eating')) {
         cat.innerHTML = `<img src="./cat-eating.png" alt="Cat Eating" />`;
+        new Audio('./cat-eating.mp3').play();
         soundBtn.disabled = true;
         soundBtn.classList.add('opacity-50', 'cursor-not-allowed');
         sleepBtn.disabled = true;
@@ -354,6 +371,7 @@ function updateUI(state) {
         cat.innerHTML = `<img src="./cat.png" alt="Cat" />`;
         loveLove.classList.add('hidden');
         celebrate.classList.add('hidden');
+        wakeUp.classList.add('hidden');
         soundBtn.disabled = false;
         sleepBtn.disabled = false;
         bathBtn.disabled = false;
@@ -365,25 +383,21 @@ function updateUI(state) {
     }
 }
 
-const feedBtn = document.getElementById('feed-btn');
-const dropdown = document.getElementById('dropdown-feed-options');
-
-document.getElementById('feed-btn').addEventListener('click', () => {
-    dropdown.classList.toggle('hidden');
-});
-
-document.addEventListener('click', (event) => {
-    if (!feedBtn.contains(event.target) && !dropdown.contains(event.target)) {
-        dropdown.classList.add('hidden');
-    }
-});
-
 // Event listeners
 document.getElementById('sound-btn').addEventListener('click', () => {
     service.send({ type: 'MEOW' });
 });
+
+const msg = document.getElementById('sleep-msg');
 document.getElementById('sleep-btn').addEventListener('click', () => {
-    service.send({ type: 'SLEEP' });
+    const { energy } = service.getSnapshot().context;
+    if (energy >= 100) {
+        msg.classList.remove('hidden');
+        setTimeout(() => msg.classList.add('hidden'), 3000);
+    } else {
+        service.send('SLEEP');
+        msg.classList.add('hidden'); // sembunyikan pesan jika sebelumnya muncul
+    }
 });
 document.getElementById('feed-whiskas').addEventListener('click', () => {
     service.send({ type: 'EAT_WHISKAS' });
@@ -396,6 +410,9 @@ document.getElementById('bath-btn').addEventListener('click', () => {
 });
 document.getElementById('cat-poop').addEventListener('click', () => {
     service.send('CLEAN_POOP');
+});
+document.getElementById('wake-up').addEventListener('click', () => {
+    service.send('WAKE_UP');
 });
 
 
